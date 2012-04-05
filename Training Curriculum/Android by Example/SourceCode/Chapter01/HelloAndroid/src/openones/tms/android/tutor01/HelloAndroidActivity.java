b81fdd8604f1e17191ac7afff0438bfd007baf6c
thachln <thachln@8c9dc9ed-9053-dc3b-3cd5-44c8541756d8>
2012-04-05 18:29:40 +0000

    Hello World by using resource

--- Training Curriculum/Android by Example/SourceCode/Chapter01/HelloAndroid/src/openones/tms/android/tutor01/HelloAndroidActivity.java
+++ Training Curriculum/Android by Example/SourceCode/Chapter01/HelloAndroid/src/openones/tms/android/tutor01/HelloAndroidActivity.java
@@ -2,16 +2,15 @@ package openones.tms.android.tutor01;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class HelloAndroidActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
-        TextView tv = new TextView(this);
-        tv.setText("Hello, Android");
-        setContentView(tv);
+        setContentView(R.layout.main);
+        // TextView tv = new TextView(this);
+        // tv.setText("Hello, Android");
+        // setContentView(tv);
     }
 }
