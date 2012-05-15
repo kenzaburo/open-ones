debd283461f1aae781ad29fe1c2e4d5e49f69ee9
thachln <thachln@8c9dc9ed-9053-dc3b-3cd5-44c8541756d8>
2012-05-15 03:23:00 +0000

    Pack and release ishare-2.1.9.1

---
+++ ProjectList/iShare/SourceCode/iShare/release/ishare-2.1.9.1/upgrade/2.1.9/mysql_2.1.8_to_2.1.9.sql
@@ -0,0 +1,3 @@
+ALTER TABLE jforum_forums ADD forum_tooltip varchar(150);
+ALTER TABLE jforum_forums ADD forum_img_url varchar(255) DEFAULT '${contextPath}/templates/${templateName}/images/folder_new_big.gif';
+ALTER TABLE jforum_forums ADD forum_new_img_url varchar(255) DEFAULT '${contextPath}/templates/${templateName}/images/folder_new_big.gif';
