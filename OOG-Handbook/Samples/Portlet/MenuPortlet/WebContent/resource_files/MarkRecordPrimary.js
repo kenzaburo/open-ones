92bc95229e228baf8ac35ccc21a3146dceb30d0c
thachln <thachln@8c9dc9ed-9053-dc3b-3cd5-44c8541756d8>
2012-07-19 09:05:36 +0000

    Removed some parts

--- OOG-Handbook/Samples/Portlet/MenuPortlet/WebContent/resource_files/MarkRecordPrimary.js
+++
@@ -1,2 +0,0 @@
-if(typeof(Sys)!=='undefined')Sys.Application.notifyScriptLoaded();
-(function() {var fn = function() {$get('ctl00_ctl00_ToolkitScriptManager1_HiddenField').value += ';';Sys.Application.remove_load(fn);};Sys.Application.add_load(fn);})();
