6a80c2bb37a2ea17f1edfabf0742efb0fa14f783
thachln <thachln@8c9dc9ed-9053-dc3b-3cd5-44c8541756d8>
2013-10-28 20:00:48 +0000

    Demo chart

--- OOG-Handbook/Samples/Portlet/PortletChart/ChartSpringPortletMVC/src/openones/tms/annospringmvcportlet/form/ChartForm.java
+++ OOG-Handbook/Samples/Portlet/PortletChart/ChartSpringPortletMVC/src/openones/tms/annospringmvcportlet/form/ChartForm.java
@@ -16,29 +16,12 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package openones.tms.annospringmvcportlet.controller;
-
-import org.apache.log4j.Logger;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+package openones.tms.annospringmvcportlet.form;
 
 /**
- * @author Thach.Le
+ * @author thachle
+ *
  */
-@Controller
-@RequestMapping("VIEW")
-public class HelloController {
-    /** Logger for logging. */
-    private static Logger log = Logger.getLogger(HelloController.class);
+public class ChartForm {
 
-    /**
-     * Default screen.
-     * @return name of view which is the name of the JSP page.
-     */
-    @RequestMapping
-    public String initScreen() {
-        log.debug("initScreen.START");
-        // Display hello.jsp
-        return "hello";
-    }
 }
